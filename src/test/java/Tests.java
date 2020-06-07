8fd53b17e6333a8c6cc501b98a056810c69d47cc
Marina Pliusnina <plyusnina.mv@gmail.com>
2020-06-07 11:57:48 +0200

    Sample test added

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,2 +1,10 @@
-public class Tests {
+import com.sun.tools.javac.util.Assert;
+import org.junit.jupiter.api.Test;
+
+class Tests {
+
+    @Test
+    void justAnExample() {
+        Assert.check(true);
+    }
 }
