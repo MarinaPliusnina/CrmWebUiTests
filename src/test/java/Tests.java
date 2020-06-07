8dc4bc397b9fc3d0c25b934ba16e88054219055a
Marina Pliusnina <plyusnina.mv@gmail.com>
2020-06-07 16:05:48 +0200

    Test Url moved to config.properties

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,15 +1,41 @@
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Tests {
+
+    private static String testUrl;
+
+    @BeforeClass
+    public static void BeforeClassInit() {
+
+        InputStream input = Tests.class.getClassLoader().getResourceAsStream("config.properties");
+
+        Properties prop = new Properties();
+
+        try{
+            prop.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        testUrl = prop.getProperty("test.url");
+    }
 
     @Test
     public void ChromeExample() {
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+
+        driver.get(testUrl);
+
         driver.quit();
     }
 
@@ -17,7 +43,9 @@ public class Tests {
     public void FirefoxExample() {
 
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.google.com/");
+
+        driver.get(testUrl);
+
         driver.quit();
     }
 }
