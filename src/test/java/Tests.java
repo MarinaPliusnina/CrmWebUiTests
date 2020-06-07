4ec386417c8684dd6163ee034e00885ea73d4419
Marina Pliusnina <plyusnina.mv@gmail.com>
2020-06-07 15:08:36 +0200

    Test for different browsers created

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,10 +1,23 @@
-import com.sun.tools.javac.util.Assert;
-import org.junit.jupiter.api.Test;
 
-class Tests {
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Tests {
 
     @Test
-    void justAnExample() {
-        Assert.check(true);
+    public void ChromeExample() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void FirefoxExample() {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
     }
 }
