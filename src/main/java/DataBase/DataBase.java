af671774d71485c1440d653e9232c66385846a2e
plyus <plyusnina.mv@gmail.com>
2020-08-17 18:04:08 +0200

    DB connection creation logic Reading connection details from config.properties

---
+++ src/main/java/DataBase/DataBase.java
@@ -0,0 +1,71 @@
+package DataBase;
+
+import config.Config;
+
+import java.sql.*;
+
+public class DataBase {
+
+    private String url;
+    private String user;
+    private String password;
+
+    Connection conn = null;
+
+    public DataBase(){
+
+        readConfig();
+
+    }
+
+    private void readConfig() {
+
+        url= "jdbc:postgresql://" +Config.getProperty("host")+":"+Config.getProperty("port")+"/"+Config.getProperty("dbname");
+        user=Config.getProperty("user");
+        password = Config.getProperty("pass");
+    }
+
+    public Connection connect() throws SQLException {
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        } return conn;
+    }
+
+    public String queryToString(String sQ) throws SQLException {
+
+        String resultString = "";
+
+        try {
+
+            Statement stat = conn.createStatement();
+
+            ResultSet result = stat.executeQuery(sQ);
+
+
+            while (result.next()) {
+
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    resultString += " " + result.getString(i);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        return resultString;
+    }
+
+
+}
